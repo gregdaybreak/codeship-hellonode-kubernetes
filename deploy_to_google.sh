352e156eaa970efd0a3946dce1367a8ef994d60f
Greg Brown <greg.brown@lootcrate.com>
2016-11-01 12:33:10 -0700

    adding

--- deploy_to_google.sh
+++ deploy_to_google.sh
@@ -3,15 +3,31 @@
 
 set -e
 
+DEFAULT_ZONE=us-west1-b
+KUBERNETES_APP_NAME=hello-node
+GOOGLE_CONTAINER_NAME=gcr.io/greg-brown-sandbox/hellonode
+
 # authenticate to google cloud
 codeship_google authenticate
 
-# set compute zone
-gcloud config set compute/zone us-west1-b
+# Set the default zone to use
+echo "Setting default compute zone $DEFAULT_ZONE"
+gcloud config set compute/zone $DEFAULT_ZONE
 
 # set kubernetes cluster
 gcloud container clusters get-credentials codeship
 
+echo "Deploying image on GCE"
+kubectl run $KUBERNETES_APP_NAME --image=$GOOGLE_CONTAINER_NAME --port=8080
+
+echo "Creating LoadBalancer to allow external traffic"
+kubectl expose deployment $KUBERNETES_APP_NAME --type="LoadBalancer"
+
+echo "Waiting for services to boot"
+
+echo "Listing services on GCE"
+kubectl get services $KUBERNETES_APP_NAME
+
 # update kubernetes Deployment
-GOOGLE_APPLICATION_CREDENTIALS=/keyconfig.json kubectl set image deployment/hellonode app=gcr.io/greg-brown-sandbox/hellonode:$CI_TIMESTAMP
+#GOOGLE_APPLICATION_CREDENTIALS=/keyconfig.json kubectl set image deployment/hellonode app=gcr.io/greg-brown-sandbox/hellonode:$CI_TIMESTAMP
 
