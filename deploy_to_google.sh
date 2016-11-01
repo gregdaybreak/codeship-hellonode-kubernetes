6dee1b746359f9b93a82294648190a95b6401d95
Greg Brown <greg.brown@lootcrate.com>
2016-11-01 10:26:35 -0700

    updating

--- deploy_to_google.sh
+++ deploy_to_google.sh
@@ -13,5 +13,5 @@ gcloud config set compute/zone us-west1-b
 gcloud container clusters get-credentials codeship
 
 # update kubernetes Deployment
-GOOGLE_APPLICATION_CREDENTIALS=/keyconfig.json kubectl set image deployment/deployment-name app=gcr.io/greg-brown-sandbox/hellonode:$CI_TIMESTAMP
+GOOGLE_APPLICATION_CREDENTIALS=/keyconfig.json kubectl set image deployment/hellonode app=gcr.io/greg-brown-sandbox/hellonode:$CI_TIMESTAMP
 
